0d43bbfc2deec0654f62a94aca224563e4a14379
sudhanshu9836 <145665877+sudhanshu9836@users.noreply.github.com>
2023-12-20 13:11:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+// adding the 'add' button
+let add = document.createElement ('button');
+add.innerText = "Add";
+add.classList.add('btn');
+document.querySelector('.l1').appendChild(add);
+
+//adding event listener for add button
+add.addEventListener("click", function(){
+    let inp = document.querySelector("#add");
+    let task = document.createElement('div');
+    task.classList.add("tasks");
+    let p = document.createElement('p');
+    p.innerText = inp.value;
+    p.style.backgroundColor = "white";
+    p.style.position = "relative";
+    p.style.bottom = "7px";
+    task.style.overflow = "hidden"
+    task.append (p);
+    let check = document.createElement("input");
+    check.type = "checkbox";
+    check.classList.add("checkbox");
+    task.prepend(check);
+    let del = document.createElement("button");
+    del.innerText = "delete";
+    del.classList.add("btn2");
+    task.append(del);
+    document.querySelector(".list").appendChild(task);
+    inp.value = "";
+    task.style.display = "flex";
+    task.style.justifyContent = "space-around"; 
+});
+
+document.addEventListener("click",function(event){
+     if (event.target.classList.contains("btn2")){
+        event.target.parentElement.remove();
+     }
+})
